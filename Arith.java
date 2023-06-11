c136145f5a963a6f6843f1fb4587c29959747fdd
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-11 09:18:36 -0700

    Create Arith.java

---
+++ Arith.java
@@ -0,0 +1,12 @@
+//Arith.java
+class Arith
+{
+	void manip(int a,int b)
+	{
+		System.out.println("Sum:"+(a+b));
+		System.out.println("Subtraction:"+(a-b));
+		System.out.println("Product:"+a*b);
+		System.out.println("Division:"+(float)a/b);
+	}
+}
+//Compile (ctrl 1)
